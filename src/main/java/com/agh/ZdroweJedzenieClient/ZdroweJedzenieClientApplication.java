a99bb237e13b700e851231c7105cdfa88378cc2e
Atloas <miter010@gmail.com>
2021-04-03 01:11:39 +0200

    Working client server?

---
+++ src/main/java/com/agh/ZdroweJedzenieClient/ZdroweJedzenieClientApplication.java
@@ -0,0 +1,14 @@
+package com.agh.ZdroweJedzenieClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class ZdroweJedzenieClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdroweJedzenieClientApplication.class, args);
+	}
+
+}
