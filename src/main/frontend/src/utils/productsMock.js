3237de6449ff6c61cb9f74780762a0ebbb171634
Grzegorz Podsiadło <grzegorz.podsiadlo@protonmail.com>
2021-04-07 17:20:17 +0200

    mocks added

---
+++ src/main/frontend/src/utils/productsMock.js
@@ -0,0 +1,26 @@
+import client from "../api/client"
+
+class productsMock {
+    productsMockGateway = "https://virtserver.swaggerhub.com/Atloas/ZdroweJedzenieAPIGateway/0.1.1/products";
+
+    getProducts() {
+        return client({
+            method: "GET",
+            path: this.productsMockGateway,
+            headers: { Accept: "application/json" },
+          }).then((response) => {
+            return response;
+          });
+    }
+    getProductById(id) {
+        return client({
+            method: "GET",
+            path: this.productsMockGateway + "/" + id,
+            headers: { Accept: "application/json" },
+          }).then((response) => {
+            return response;
+          });
+    }
+}
+
+export default productsMock;
