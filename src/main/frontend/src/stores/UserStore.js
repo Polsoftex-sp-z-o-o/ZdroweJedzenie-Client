a2c115071e128c163c4ba2e551e48b4b5d6c1a60
micpz <kielski@student.agh.edu.pl>
2021-05-11 22:49:14 +0200

    add token to UserStore

--- src/main/frontend/src/stores/UserStore.js
+++ src/main/frontend/src/stores/UserStore.js
@@ -5,7 +5,12 @@ class UserStore {
         extendObservable(this, {
             loading: true,
             isLoggedIn: false,
-            username: ''
+            
+            email: '',
+            firstName: '',
+            lastName: '',
+            token: null
+
         })
     }
 }
