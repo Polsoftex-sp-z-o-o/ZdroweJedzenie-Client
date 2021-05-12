42e44bc4e2339aa46186d3fda526adf4fb10a98a
jerzyklos <jerzyklosik@interia.pl>
2021-05-12 19:37:41 +0200

    elo

--- src/main/frontend/src/components/OrderSummary.js
+++ src/main/frontend/src/components/OrderSummary.js
@@ -14,45 +14,45 @@ class OrderSummary extends React.Component {
 
   render() {
     return (
+		<div className='blur_background_out'>
+			<div className='order_summary'>
+			  <div className='order_summary_inner'>
+				  <form className="register_form" onSubmit={this.handleSubmit}>
+				  <label className="summary_title">Płatność</label>
 
-        <div className='order_summary'>
-          <div className='order_summary_inner'>
-            <h1>{this.props.text}</h1>
-              <form className="register_form" onSubmit={this.handleSubmit}>
-              <label className="summary_title">Płatność</label>
-
-              <label className="register_form__label">Numer karty</label>
-              <input
-                className="register_form__input"
-                name="address"
-                type="text"
-              />
-              <label className="register_form__label">Data ważności</label>
-              <input
-                className="register_form__input"
-                name="address"
-                type="text"
-              />
-              <label className="register_form__label">Kod</label>
-              <input
-                className="register_form__input"
-                name="address"
-                type="text"
-              />
-              <input
-                className="register_form__submit"
-                type="submit"
-                value="Zamów"
-              />
-              <input
-                className="register_form__submit"
-                type="submit"
-                value="Wróć"
-                onClick={this.props.closePopup}
-              />
-            </form>
-          </div>
-      </div>
+				  <label className="register_form__label">Numer karty</label>
+				  <input
+					className="register_form__input"
+					name="address"
+					type="text"
+				  />
+				  <label className="register_form__label">Data ważności</label>
+				  <input
+					className="register_form__input"
+					name="address"
+					type="text"
+				  />
+				  <label className="register_form__label">Kod</label>
+				  <input
+					className="register_form__input"
+					name="address"
+					type="text"
+				  />
+				  <input
+					className="order_form__submit"
+					type="submit"
+					value="Zamów"
+				  />
+				  <input
+					className="order_form__return"
+					type="submit"
+					value="Wróć"
+					onClick={this.props.closePopup}
+				  />
+				</form>
+			  </div>
+		  </div>
+		</div>
       );
   }
 }
