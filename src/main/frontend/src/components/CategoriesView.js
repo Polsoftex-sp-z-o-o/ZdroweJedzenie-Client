2586775441c6edc776011b78b388e93d4f39b6c6
jerzyklos <jerzyklosik@interia.pl>
2021-06-17 01:07:31 +0200

    Categories view

--- src/main/frontend/src/components/CategoriesView.js
+++ src/main/frontend/src/components/CategoriesView.js
@@ -5,9 +5,67 @@ class CategoriesView extends React.Component {
     super(props);
     this.categories = [];
     this.state = {};
+    this.categories_checkbox = [];
+    var categories_from_products = [];
+    categories_from_products.push("Wypieki");
+    categories_from_products.push("Dzemy, miody i kremy");
+    categories_from_products.push("Przetwory owocowe i warzywne");
+    categories_from_products.push("Bakalie");
+    categories_from_products.push("Warzywa i owoce");
+    categories_from_products.push("Oleje, oliwy, octy");
+    for(var category in categories_from_products){
+      if(categories_from_products[category]==="Wypieki"){
+        this.categories_checkbox.push(
+            <label className="categories_container">
+            {categories_from_products[category]}
+            <input type="checkbox" onChange={this.handleSelectOnChangeWypieki} />
+            <span className="categories_checkmark"></span>
+          </label>);
+      }
+      else if(categories_from_products[category]==="Dzemy, miody i kremy"){
+        this.categories_checkbox.push(
+          <label className="categories_container">
+          {categories_from_products[category]}
+          <input type="checkbox" onChange={this.handleSelectOnChangeDzemy} />
+          <span className="categories_checkmark"></span>
+        </label>);
+      }
+      else if(categories_from_products[category]==="Przetwory owocowe i warzywne"){
+        this.categories_checkbox.push(
+          <label className="categories_container">
+          {categories_from_products[category]}
+          <input type="checkbox" onChange={this.handleSelectOnChangePrzetwory} />
+          <span className="categories_checkmark"></span>
+        </label>);
+      }
+      else if(categories_from_products[category]==="Bakalie"){
+        this.categories_checkbox.push(
+          <label className="categories_container">
+          {categories_from_products[category]}
+          <input type="checkbox" onChange={this.handleSelectOnChangeBakalie} />
+          <span className="categories_checkmark"></span>
+        </label>);
+      }
+      else if(categories_from_products[category]==="Warzywa i owoce"){
+        this.categories_checkbox.push(
+          <label className="categories_container">
+          {categories_from_products[category]}
+          <input type="checkbox" onChange={this.handleSelectOnChangeOwoce} />
+          <span className="categories_checkmark"></span>
+        </label>);
+      }
+      else if(categories_from_products[category]==="Oleje, oliwy, octy"){
+        this.categories_checkbox.push(
+          <label className="categories_container">
+          {categories_from_products[category]}
+          <input type="checkbox" onChange={this.handleSelectOnChangeOleje} />
+          <span className="categories_checkmark"></span>
+        </label>);
+      }
+    }
   }
 
-  handleSelectOnChangeDairy = (event) => {
+  handleSelectOnChangeWypieki = (event) => {
     if (event.target.checked) this.categories.push("Wypieki");
     else {
       var categorieIndex = this.categories.indexOf("Wypieki");
@@ -16,28 +74,46 @@ class CategoriesView extends React.Component {
     this.props.parentHandler(this.categories);
   };
 
-  handleSelectOnChangeFruit = (event) => {
-    if (event.target.checked) this.categories.push("Fruit");
+  handleSelectOnChangeDzemy = (event) => {
+    if (event.target.checked) this.categories.push("Dzemy, miody i kremy");
     else {
-      var categorieIndex = this.categories.indexOf("Fruit");
+      var categorieIndex = this.categories.indexOf("Dzemy, miody i kremy");
       this.categories.splice(categorieIndex, 1);
     }
     this.props.parentHandler(this.categories);
   };
 
-  handleSelectOnChangeVegetable = (event) => {
-    if (event.target.checked) this.categories.push("Vegetable");
+  handleSelectOnChangePrzetwory = (event) => {
+    if (event.target.checked) this.categories.push("Przetwory owocowe i warzywne");
     else {
-      var categorieIndex = this.categories.indexOf("Vegetable");
+      var categorieIndex = this.categories.indexOf("Przetwory owocowe i warzywne");
       this.categories.splice(categorieIndex, 1);
     }
     this.props.parentHandler(this.categories);
   };
 
-  handleSelectOnChangeMeat = (event) => {
-    if (event.target.checked) this.categories.push("Meat");
+  handleSelectOnChangeBakalie = (event) => {
+    if (event.target.checked) this.categories.push("Bakalie");
     else {
-      var categorieIndex = this.categories.indexOf("Meat");
+      var categorieIndex = this.categories.indexOf("Bakalie");
+      this.categories.splice(categorieIndex, 1);
+    }
+    this.props.parentHandler(this.categories);
+  };
+
+  handleSelectOnChangeOwoce = (event) => {
+    if (event.target.checked) this.categories.push("Warzywa i owoce");
+    else {
+      var categorieIndex = this.categories.indexOf("Warzywa i owoce");
+      this.categories.splice(categorieIndex, 1);
+    }
+    this.props.parentHandler(this.categories);
+  };
+
+  handleSelectOnChangeOleje = (event) => {
+    if (event.target.checked) this.categories.push("Oleje, oliwy, octy");
+    else {
+      var categorieIndex = this.categories.indexOf("Oleje, oliwy, octy");
       this.categories.splice(categorieIndex, 1);
     }
     this.props.parentHandler(this.categories);
@@ -48,32 +124,7 @@ class CategoriesView extends React.Component {
       <div className="categories_box">
         <div className="categories_title">Kategorie</div>
         <div className="categories_checkboxes">
-          <label className="categories_container">
-            Wypieki
-            <input type="checkbox" onChange={this.handleSelectOnChangeDairy} />
-            <span className="categories_checkmark"></span>
-          </label>
-
-          <label className="categories_container">
-            Fruit
-            <input type="checkbox" onChange={this.handleSelectOnChangeFruit} />
-            <span className="categories_checkmark"></span>
-          </label>
-
-          <label className="categories_container">
-            Vegetable
-            <input
-              type="checkbox"
-              onChange={this.handleSelectOnChangeVegetable}
-            />
-            <span className="categories_checkmark"></span>
-          </label>
-
-          <label className="categories_container">
-            Meat
-            <input type="checkbox" onChange={this.handleSelectOnChangeMeat} />
-            <span className="categories_checkmark"></span>
-          </label>
+          <div>{this.categories_checkbox}</div>
         </div>
       </div>
     );
--- src/main/frontend/src/components/ProductsView.js
+++ src/main/frontend/src/components/ProductsView.js
@@ -20,13 +20,14 @@ class ProductsView extends React.Component {
     this.handleSearch = this.handleSearch.bind(this);
     this.handleCategories = this.handleCategories.bind(this);
     console.log(UserStore.isAdmin);
-    // const isAdmin =
-    //   UserStore.authorities && UserStore.authorities.includes("ROLE_ADMIN");
-    // console.log("Czy to admin", isAdmin);
+
+    this.getCategories = this.getCategories.bind(this);
 
     this.state = {
       products: [],
       filteredProducts: [],
+      toogleRefresh: false,
+      categories: [],
     };
   }
 
@@ -52,6 +53,32 @@ class ProductsView extends React.Component {
         products: newProducts,
         filteredProducts: newProducts,
       });
+
+      console.log(response);
+      console.log(this.state.products);
+      this.getCategories();
+    } catch (err) {
+      console.warn(err);
+      alert("Nie udało się załadować produktów");
+    }
+
+    try {
+      const response = await axios.get(
+        "http://zdrowejedzenie.44b0bdc6651241b0874a.eastus.aksapp.io/gateway/products/getAllCategories/",
+        null,
+        { "Content-Type": "application/json" }
+      );
+      //const newProducts = response.data;
+
+      console.log(response.data);
+      this.setState({
+        categories: response.data,
+      });
+      // this.setState({
+      //   ...this.state,
+      //   products: newProducts,
+      //   filteredProducts: newProducts,
+      // });
       // console.log(response);
       // console.log(this.state.products);
     } catch (err) {
@@ -104,6 +131,7 @@ class ProductsView extends React.Component {
     this.setState({
       ...this.state,
       filteredProducts: this.filterProductsByCategories(categories),
+      categories_from_products: this.getCategories(),
     });
   }
 
@@ -145,6 +173,17 @@ class ProductsView extends React.Component {
     return output;
   }
 
+  getCategories() {
+    var categories = [];
+    this.state.products.forEach(function (product) {
+      if (!categories.includes(product.category))
+        categories.push(product.category);
+    });
+    console.log("KATEGORIE");
+    console.log(categories);
+    return categories;
+  }
+
   render() {
     const products = this.state.filteredProducts.map((product) => {
       // console.log(product);
@@ -167,9 +206,14 @@ class ProductsView extends React.Component {
 
     return (
       <div>
-        <CategoriesView parentHandler={this.handleCategories} />
-
-        <Search parentHandler={this.handleSearch} />
+        <CategoriesView
+          parentHandler={this.handleCategories}
+          categories_from_products={this.state.categories}
+        />
+        <Search
+          parentHandler={this.handleSearch}
+          categories={this.state.categories}
+        />
 
         <div className="row">
           {UserStore.isAdmin && (
