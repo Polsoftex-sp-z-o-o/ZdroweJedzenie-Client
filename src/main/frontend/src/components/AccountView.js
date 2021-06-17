cf8ef1f50834b3885a641d55f6c962867b13ba25
Grzegorz Podsiadło <grzegorz.podsiadlo@protonmail.com>
2021-06-17 02:17:36 +0200

    add getting account view details from gateway

--- src/main/frontend/src/components/AccountView.js
+++ src/main/frontend/src/components/AccountView.js
@@ -1,40 +1,80 @@
-import UsersMock from "../utils/usersMock";
 import React from "react";
 import HistoryOrders from "./HistoryOrders";
+import { decodeToken } from "react-jwt";
+import UserStore from "../stores/UserStore";
+import axios from "axios";
 
 class AccountView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       id: "",
-      name: "",
+      firstName: "",
+      lastName: "", 
       address: "",
+      roles: [{ name: ""},],
     };
   }
 
   componentDidMount() {
-    this.getUserInformation().then((result) =>
-      this.setState({
-        id: result.entity.id,
-        name: result.entity.name,
-        address: result.entity.address,
-      })
-    );
+    this.getUserInformation();
   }
 
-  getUserInformation() {
-    let user = new UsersMock();
-    return user.getUserById(0);
+  async getUserInformation() {
+    try {
+      const apiURL =
+        "http://zdrowejedzenie.44b0bdc6651241b0874a.eastus.aksapp.io/gateway/";
+      const token = UserStore.token;
+      const decodedToken = decodeToken(token);
+      console.log(decodedToken["user-id"]);
+      const authAxios = axios.create({
+        baseURL: apiURL,
+        headers: {
+          Authorization: token,
+        },
+      });
+      const response = await authAxios.get(
+        apiURL + "users/" + decodedToken["user-id"] + "/",
+        null,
+        { "Content-Type": "application/json" }
+      );
+
+      const userInformation = response.data;
+      console.log(userInformation);
+      this.setState({
+        id: userInformation.id,
+        firstName: userInformation.firstName,
+        lastName: userInformation.lastName, 
+        address: userInformation.address,
+        roles: userInformation.roles,
+      });
+    } catch (err) {
+      console.warn(err);
+      alert("Nie udało się załadować użytkownika!");
+    }
+   
   }
+
+
 
   render() {
+    var roleBadge = (
+      <span className="badge rounded-pill bg-success">Klient</span>
+    );
+    // TODO: swap based on role
+    // <span className="badge rounded-pill bg-warning">Admin</span>
+
     return (
       <div className="card">
         <div className="card-body">
-          <h1 className="card-title">Twoje dane: </h1>
+          <h1 className="card-title"> {roleBadge} Twoje dane: </h1>
           <h2 className="card-subtitle mb-2 text-muted">
             {" "}
-            Godność: {this.state.name}{" "}
+            Imię: {this.state.firstName}{" "}
+          </h2>
+          <h2 className="card-subtitle mb-2 text-muted">
+            {" "}
+            Nazwisko: {this.state.lastName}{" "}
           </h2>
           <h2 className="card-text">
             {" "}
